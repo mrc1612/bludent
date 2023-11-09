b617cff38f7d6d025324bd76e3d42db46cbdd62d
Lucas Fritzke <86270203+lucasfritzke@users.noreply.github.com>
2023-11-09 20:03:21 -0300

    Primeiro Commit

---
+++ src/main/java/com/lucfritzke/bludent/BludentApplication.java
@@ -0,0 +1,13 @@
+package com.lucfritzke.bludent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BludentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BludentApplication.class, args);
+	}
+
+}
