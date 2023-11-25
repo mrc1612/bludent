064aec4dc454c0b0a0795bef5c97f4269747ab5e
Lucas Fritzke <86270203+lucasfritzke@users.noreply.github.com>
2023-11-25 15:14:42 -0300

    Push nova Branch

--- Sql/ComandosSql.sql
+++ Sql/ComandosSql.sql
@@ -7,5 +7,5 @@ select * from dentista;
 select * from procedimento;
 select * from paciente;
 select * from procedimento_dentista;
-
+/*sds*/
 select  distinct p.cd_procedimento, p.ds_procedimento from procedimento p inner join procedimento_dentista pd on p.cd_procedimento = pd.cd_procedimento;
